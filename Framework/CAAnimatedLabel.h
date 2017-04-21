1903c49f309c38857a5717583d4f0d25314c3349
chenao <jim@chenaodeMacBook-Air.local>
2017-04-21 11:27:44 +0800

    modify framework

---
+++ Framework/CAAnimatedLabel.h
@@ -0,0 +1,23 @@
+//
+//  CAAnimatedLabel.h
+//  CAAnimatedLabel
+//
+//  Created by chenao on 17/4/21.
+//  Copyright © 2017年 chenao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CAAnimatedLabel.
+FOUNDATION_EXPORT double CAAnimatedLabelVersionNumber;
+
+//! Project version string for CAAnimatedLabel.
+FOUNDATION_EXPORT const unsigned char CAAnimatedLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CAAnimatedLabel/PublicHeader.h>
+
+#import<CAAnimatedLabel/CAAnimateLabel.h>
+#import<CAAnimatedLabel/CAAnimateLabel+Animation.h>
+#import<CAAnimatedLabel/CAAnimateUtil.h>
+#import<CAAnimatedLabel/CATextAttribute.h>
+#import<CAAnimatedLabel/CATextLayout.h>
